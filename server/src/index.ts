1d95a18f5fea298874043ac53877fc13f704427a
vdediego <vdediego@atsistemas.com>
2020-02-26 17:42:40 +0100

    * Implement squeleton Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-import express from 'express';
+import express, {Application} from 'express';
+
+class Server {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+    }
+
+    config():void {
+        // Server config
+    }
+
+    routes(): void {
+        // Server routing
+    }
+
+    start(): void {
+        // Server start listening
+    }
+}
+
+new Server();
