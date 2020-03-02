2a6bb460edf102a6459c76e60908ec0c2b7ce043
vdediego <vdediego@atsistemas.com>
2020-03-02 11:19:06 +0100

    * Adapt database schema

--- database/database.sql
+++ database/database.sql
@@ -1,55 +1,48 @@
 USE agcourse;
 
-CREATE TABLE plants(
-    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    plant_image_id INT(20) NOT NULL,
-    family_id INT(20) NOT NULL,
-    category_id INT(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+DROP TABLE plants;
+DROP TABLE families;
+DROP TABLE categories;
+DROP TABLE images;
 
-CREATE TABLE families(
-    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE categories(
-    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE plant_images(
-    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    plant_id INT(20) NOT NULL,
-    image_id INT(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 CREATE TABLE images(
     id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     caption VARCHAR(255) NOT NULL,
     filename VARCHAR(100) NOT NULL,
-    plant_image_id INT(20) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE plants
-ADD FOREIGN KEY plant_image_id REFERENCES plant_images(id);
 
-ALTER TABLE plants
-ADD FOREIGN KEY family_id REFERENCES families(id);
+CREATE TABLE categories(
+    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_id INT(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (image_id) REFERENCES images(id)
+);
 
-ALTER TABLE plants
-ADD FOREIGN KEY category_id REFERENCES categories(id);
 
-ALTER TABLE plant_images
-ADD FOREIGN KEY image_id REFERENCES images(id);
+CREATE TABLE families(
+    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_id INT(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (image_id) REFERENCES images(id)
+);
 
-ALTER TABLE plant_images
-ADD FOREIGN KEY plant_id REFERENCES plants(id);
+
+CREATE TABLE plants(
+    id INT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_id INT(20) NOT NULL,
+    family_id INT(20) NOT NULL,
+    category_id INT(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (image_id) REFERENCES images(id),
+    FOREIGN KEY (family_id) REFERENCES families(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
